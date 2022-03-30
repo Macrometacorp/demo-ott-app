792f144abe9287346cca6d3bbde41d6a8bc98c67
naimishvora@macrometa.com <naimishvora@macrometa.com>
2022-03-30 13:30:50 +0530

    Removed login/signup page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,19 +6,15 @@ import Navbar from "./components/Navbar/Navbar"
 import Homepage from "./pages/Homepage/Homepage"
 import Movies from "./pages/Movies/Movies"
 import TVSeries from "./pages/TVSeries/TVSeries"
-import MyList from "./pages/MyList/MyList"
-import Auth from "./pages/Auth/Auth"
 import Search from "./pages/Search/Search"
 import Category from "./pages/Category/Category"
 import DetailModal from "./components/DetailModal/DetailModal"
 import SplashAnimation from "./components/SplashAnimation/SplashAnimation"
 import PlayAnimation from "./components/PlayAnimation/PlayAnimation"
-import { selectCurrentUser } from "./redux/auth/auth.selectors"
 import { selectSearchResults } from "./redux/search/search.selectors"
 import { checkUserSession } from "./redux/auth/auth.actions"
 
 const App = () => {
-    const currentUser = useSelector(selectCurrentUser)
     const searchResults = useSelector(selectSearchResults)
     const dispatch = useDispatch()
     const location = useLocation()
@@ -29,45 +25,24 @@ const App = () => {
 
     return (
         <div className="App">
-            {currentUser && (
-                <>
-                    <Navbar />
-                    <DetailModal />
-                </>
-            )}
+            <>
+                <Navbar />
+                <DetailModal />
+            </>
             {/* <AnimatePresence exitBeforeEnter> */}
             <Switch location={location} key={location.pathname}>
                 <Route exact path="/">
-                    <Redirect to="/login" />
+                    <Redirect to="/browse" />
                 </Route>
                 <Route path="/splash" component={SplashAnimation} />
                 <Route path="/play" component={PlayAnimation} />
-                <Route
-                    path="/search"
-                    render={() =>
-                        currentUser ? searchResults && <Search results={searchResults} /> : <Redirect to="/login" />
-                    }
-                />
-                <Route exact path="/browse" render={() => (currentUser ? <Homepage /> : <Redirect to="/login" />)} />
-                <Route
-                    exact
-                    path="/browse/:categoryName"
-                    render={(props) => (currentUser ? <Category {...props} /> : <Redirect to="/login" />)}
-                />
-                <Route exact path="/tvseries" render={() => (currentUser ? <TVSeries /> : <Redirect to="/login" />)} />
-                <Route
-                    exact
-                    path="/tvseries/:categoryName"
-                    render={(props) => (currentUser ? <Category {...props} /> : <Redirect to="/login" />)}
-                />
-                <Route exact path="/movies" render={() => (currentUser ? <Movies /> : <Redirect to="/login" />)} />
-                <Route
-                    exact
-                    path="/movies/:categoryName"
-                    render={(props) => (currentUser ? <Category {...props} /> : <Redirect to="/login" />)}
-                />
-                <Route exact path="/mylist" render={() => (currentUser ? <MyList /> : <Redirect to="/login" />)} />
-                <Route exact path="/login" render={() => (currentUser ? <Redirect to="/splash" /> : <Auth />)} />
+                <Route path="/search" render={() => searchResults && <Search results={searchResults} />} />
+                <Route exact path="/browse" render={() => <Homepage />} />
+                <Route exact path="/browse/:categoryName" render={(props) => <Category {...props} />} />
+                <Route exact path="/tvseries" render={() => <TVSeries />} />
+                <Route exact path="/tvseries/:categoryName" render={(props) => <Category {...props} />} />
+                <Route exact path="/movies" render={() => <Movies />} />
+                <Route exact path="/movies/:categoryName" render={(props) => <Category {...props} />} />
                 <Route path="*">
                     <Redirect to="/" />
                 </Route>
